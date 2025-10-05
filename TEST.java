6da68636981256fd2e22cc13381b6b89ba26eea1
momomojun <2058173210@qq.com>
2025-10-04 18:28:39 -0700

    Initial commit

---
+++ TEST.java
@@ -0,0 +1,12 @@
+public class TEST {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        
+        // 示例：变量与计算
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+    }
+}
+
