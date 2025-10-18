bc5410857e2f75a753b04635bc51e50acf0c252c
momomojun <2058173210@qq.com>
2025-10-17 20:17:26 -0700

    test maven project

---
+++ java_projects/myproject/src/main/java/edu/neu/mgen/App.java
@@ -0,0 +1,12 @@
+package edu.neu.mgen;
+
+/**
+ * Hello world!
+ */
+// jun zhai first commit on java maven projecgt
+//this is my second commit to test
+public class App {
+    public static void main(String[] args) {
+        System.out.println("It is my first Java program");
+    }
+}
